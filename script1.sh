47e2856ec809dbd59abd943932cce4ad09b916e8
lucasmolpe03-stack <lucmolper3@gmail.com>
2025-10-29 19:19:17 +0100

    solo vpc funcional

--- script1.sh
+++ script1.sh
@@ -1,3 +1,12 @@
-aws ec2 create-vpc --cidr-block 192.168.0.0/24 --query Vpc.VpcId --output text \
+#creo la vpc y devuelvo su id
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 --query Vpc.VpcId --output text \
     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-    --query Vpc.VpcId --output text 
+    --query Vpc.VpcId --output text)
+
+#muestro el id de la vpc
+echo $VPC_ID
+
+#habilitodns en la vpc
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
