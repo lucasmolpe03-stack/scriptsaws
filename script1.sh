4a0b15bab38ed1ba5f799f37241042529506d8ac
lucasmolpe03-stack <lucmolper3@gmail.com>
2025-10-29 18:42:43 +0100

    script1 creo vpc

--- script1.sh
+++ script1.sh
@@ -1,3 +1,3 @@
 aws ec2 create-vpc --cidr-block 192.168.0.0/24 --query Vpc.VpcId --output text \
     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-    --query Vpc.VpcId --output text
+    --query Vpc.VpcId --output text 
